29ed4a2008bdda73d9d7e7db56ed30f31ffa29ed
Jason W. Bacon <bacon4000@gmail.com>
2025-03-27 12:18:55 -0500

    Set LANG in MultiQC scripts

--- Yeast/07-multiqc-trimmed.sh
+++ Yeast/07-multiqc-trimmed.sh
@@ -16,6 +16,7 @@
 # multiqc: LC_ALL and LANG must be set to a UTF-8 character set
 # in your environment in order for the click module to function.
 export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
 
 input_dir=Results/06-qc-trimmed
 output_dir=Results/07-multiqc-trimmed

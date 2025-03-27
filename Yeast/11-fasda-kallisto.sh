8490d28d2efd28fea3f93cdfa27a4a6b11ea7fbf
Jason Bacon <bacon@moray.acadix.biz>
2025-03-27 10:20:57 -0500

    Add 11-fasda-kallisto

---
+++ Yeast/11-fasda-kallisto.sh
@@ -0,0 +1,44 @@
+#!/bin/sh -e
+
+##########################################################################
+#   Description:
+#       Run fasda normalize and fold-change on kallisto abundances
+##########################################################################
+
+input_dir=Results/10-kallisto-quant
+samples=$(ls $input_dir | wc -l)
+replicates=$(($samples / 2))
+
+uname -a
+fasda --version
+pwd
+
+output_dir=Results/11-fasda-kallisto
+mkdir -p $output_dir
+cd $output_dir
+
+for condition in 1 2; do
+    norm_file=cond$condition-all-norm-$replicates.tsv
+    printf "Normalizing condition $condition: $replicates replicates\n"
+    files=""
+    for r in $(seq 1 $replicates); do
+	r2=$(printf "%02d" $r)
+	files="$files ../../$input_dir/sample*cond$condition-rep$r2*/abundance.tsv"
+    done
+    echo "time fasda normalize --output $norm_file $files"
+    time fasda normalize --output $norm_file $files
+    printf "\nCondition $condition normalized counts:\n\n"
+    head $norm_file
+done
+
+de_file=fc-$replicates-replicates.txt
+printf "\nComputing fold-change for $replicates replicates...\n"
+echo "time fasda fold-change --output $de_file cond1-all-norm-$replicates.tsv cond2-all-norm-$replicates.tsv"
+time fasda fold-change \
+     --output $de_file \
+    cond1-all-norm-$replicates.tsv cond2-all-norm-$replicates.tsv
+
+more $de_file
+printf "\n%-25s %10s %10s\n" "File" "Features" "P < 0.05"
+printf "%-25s %10s %10s\n" $de_file: \
+	$(cat $de_file | wc -l) $(awk '$8 < 0.05' $de_file | wc -l)

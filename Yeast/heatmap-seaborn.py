4ee7b7f877139e79218c531ee0c7262434f722a1
Jason Bacon <bacon@moray.acadix.biz>
2025-04-01 16:40:16 -0500

    Filter transcripts to produce a more meaningful heatmap

--- Yeast/heatmap-seaborn.py
+++ Yeast/heatmap-seaborn.py
@@ -16,7 +16,7 @@ seaborn.set_style("whitegrid")
 
 counts = pandas.read_csv('small.tsv',sep='\t')
 print(counts.shape)
-print(counts.head())
+print(counts)
 print(counts.describe())
 
 row_medians = counts.median(axis=1,numeric_only=True)
@@ -28,10 +28,9 @@ for col in counts.columns[1:]:
     counts_row_norm[col] = numpy.log2((counts[col]+0.1)/(row_medians+0.1))
 print(counts_row_norm.describe())
 # print(counts_row_norm.iloc[6,1:].median()) #test some random rows, make sure the median value is 0
-print(counts_row_norm.head())
+print(counts_row_norm)
 
 counts_row_norm = counts_row_norm.set_index('Transcript')
-
-#seaborn.clustermap(counts_row_norm)
-seaborn.clustermap(counts_row_norm, cmap="RdBu_r", figsize=(10,10))
+# seaborn.clustermap(counts_row_norm)
+seaborn.clustermap(counts_row_norm, col_cluster=False, cmap="RdBu_r", figsize=(10,10))
 plt.show()
--- Yeast/heatmap.sh
+++ Yeast/heatmap.sh
@@ -36,6 +36,20 @@ usage()
 
 
 ##########################################################################
+#   Function description:
+#       Pause until user presses return
+##########################################################################
+
+pause()
+{
+    local junk
+    
+    printf "Press return to continue..."
+    read junk
+}
+
+
+##########################################################################
 #   Main
 ##########################################################################
 
@@ -47,22 +61,24 @@ fi
 features=filtered-features.txt
 fasda filter --max-p-val 0.05 Results/11-fasda-kallisto/fc-3-replicates.txt \
     | awk '{ print $1 }' > $features
+wc -l $features
+fgrep -f $features Results/11-fasda-kallisto/cond1-norm-3.tsv > cond1-norm-3.tsv
+fgrep -f $features Results/11-fasda-kallisto/cond2-norm-3.tsv > cond2-norm-3.tsv
+pause
 
 # Concatenate corresponding lines from the normalized counts files
-# and remove the 2nd occurrence of the feature name
+# and remove the 2nd occurrence of the feature name (column 5 in pasted output)
 counts=counts.tsv
 rm -f $counts
-paste Results/11-fasda-kallisto/cond1-norm-3.tsv \
-    Results/11-fasda-kallisto/cond2-norm-3.tsv \
+paste cond1-norm-3.tsv cond2-norm-3.tsv \
     | awk '{ printf("%s\t%s\t%s\t%s\t%s\t%s\t%s\n", $1, $2, $3, $4, $6, $7, $8, $9); }' \
     > $counts
-head $counts
 
 # Generate example small file
 # To-do: Convert counts to z-scores for realistic color comparison across
 # features with different expression levels
 printf "Transcript\tc1r1\tc1r2\tc1r3\tc2r1\tc2r2\tc2r3\n" > small.tsv
-head $counts >> small.tsv
+head -n 30 $counts >> small.tsv
 cat small.tsv
 
 # Load the counts into gnuplot and draw a heatmap

e449f1833a78aaea2fc42bed44ec2fa4ed75602a
Jason Bacon <bacon@moray.acadix.biz>
2025-04-11 17:24:46 -0500

    Change FC output filename

--- Yeast/14c-fasda-fc-hisat2.sh
+++ Yeast/14c-fasda-fc-hisat2.sh
@@ -8,13 +8,14 @@ uname -a
 fasda --version
 pwd
 
+outfile=fc-3-replicates.txt
 export PATH=~/Prog/Src/local/bin:$PATH
 printf "Computing fold-changes...\n"
 set -x
-fasda fold-change --debug --output fc.txt cond1-norm.tsv cond2-norm.tsv
+fasda fold-change --output $outfile cond1-norm.tsv cond2-norm.tsv
 set +x
 
-more fc.txt
+more $outfile
 printf "\n%-25s %10s %10s\n" "File" "Features" "P < 0.05"
-printf "%-25s %10s %10s\n" fc.txt: \
-	$(cat fc.txt | wc -l) $(awk '$8 < 0.05' fc.txt | wc -l)
+printf "%-25s %10s %10s\n" $outfile: \
+	$(cat $outfile | wc -l) $(awk '$8 < 0.05' $outfile | wc -l)

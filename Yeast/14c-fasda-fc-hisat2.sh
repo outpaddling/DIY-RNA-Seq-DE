6780949b03b7bbddee7bcb1fe8a8e3ae20196640
Jason W. Bacon <bacon4000@gmail.com>
2025-04-15 12:14:58 -0500

    14c-fasda-fc-hisat2.sh: Align with 11b-fasda-fc

--- Yeast/14c-fasda-fc-hisat2.sh
+++ Yeast/14c-fasda-fc-hisat2.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -e
 
+input_dir=Results/13-hisat2-align
+samples=$(ls $input_dir/*.bam | wc -l)
+replicates=$(($samples / 2))
+
 output_dir=Results/14-fasda-hisat2
 mkdir -p $output_dir
 cd $output_dir
@@ -9,7 +13,7 @@ fasda --version
 pwd
 
 # Separate counts by condition for fasda fold-change
-norm_all=all-norm.tsv
+norm_all=all-norm-$replicates.tsv
 printf "\nCondition 1 normalized counts:\n"
 norm_file1=cond1-norm.tsv
 cut -f 1-4 $norm_all > $norm_file1
@@ -22,10 +26,9 @@ head -n 5 $norm_file2
 
 outfile=fc-3.txt
 # export PATH=~/Prog/Src/local/bin:$PATH
-printf "Computing fold-changes...\n"
-set -x
-fasda fold-change --output $outfile cond1-norm.tsv cond2-norm.tsv
-set +x
+printf "\nComputing fold-change for $replicates replicates...\n"
+echo "time fasda fold-change --output $outfile cond1-norm-$replicates.tsv cond2-norm-$replicates.tsv"
+time fasda fold-change --output $outfile cond1-norm.tsv cond2-norm.tsv
 
 more $outfile
 printf "\n%-25s %10s %10s\n" "File" "Features" "P < 0.05"

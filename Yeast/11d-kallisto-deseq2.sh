2db069a0cba7207d8951e1fd1a2564a7aacfff65
Jason Bacon <bacon@moray.acadix.biz>
2025-05-02 08:09:03 -0500

    11d-kallisto-deseq2.sh: Prep and run DESeq2

---
+++ Yeast/11d-kallisto-deseq2.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -e
+
+# FIXME: This only currently works for 3 samples
+
+# FIXME: Move creation of counts.tsv to kallisto-deseq2.R so that
+# this is more representative of a typical DESeq2 analysis.
+cut -f 1 Results/10-kallisto-quant/sample01*/abundance.tsv > counts.tsv
+sample=1
+for file in Results/10-kallisto-quant/*/abundance.tsv; do
+    cut -f 4 $file | sed -e "s|est_counts|s$sample|" | paste counts.tsv - > temp.tsv
+    mv temp.tsv counts.tsv
+    sample=$(($sample + 1))
+done
+head counts.tsv
+./kallisto-deseq2.R
+
+de_file=deseq2-results.tsv
+printf "\n%-25s %10s %10s %11s\n" "File" "Features" "P < 0.05" "Padj < 0.05"
+printf "%-25s %10s %10s %11s\n" $de_file: \
+	$(cat $de_file | wc -l) \
+	$(awk '$5 < 0.05' $de_file | wc -l) \
+	$(awk '$6 < 0.05' $de_file | wc -l)
+

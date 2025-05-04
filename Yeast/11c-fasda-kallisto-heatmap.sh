b618a1d26825ab1942c397689380467d66dc1b1c
Jason W. Bacon <bacon4000@gmail.com>
2025-05-04 15:42:17 -0500

    11c-fasda-kallisto-heatmap.sh: Fix to work with more than 3 replicates

--- Yeast/11c-fasda-kallisto-heatmap.sh
+++ Yeast/11c-fasda-kallisto-heatmap.sh
@@ -9,13 +9,17 @@
 #   2025-03-31  Jason Bacon Begin
 ##########################################################################
 
+input_dir=Results/10-kallisto-quant
+samples=$(ls $input_dir | wc -l)
+replicates=$(($samples / 2))
+
 # Generate a list of features of interest by filtering the FASDA fold-changes
 features=filtered-features.txt
-fasda filter --max-p-val 0.05 Results/11-fasda-kallisto/fc-3.txt \
+fasda filter --max-p-val 0.05 Results/11-fasda-kallisto/fc-$replicates.txt \
     | awk '{ print $1 }' | head -n 30 > $features
 wc -l $features
 
 # FIXME: Update when ./heatmap is moved into PATH
 # Add --debug to see Python data structures
-fasda heatmap $features Results/11-fasda-kallisto/cond1-norm-3.tsv \
-    Results/11-fasda-kallisto/cond2-norm-3.tsv
+fasda heatmap $features Results/11-fasda-kallisto/cond1-norm-$replicates.tsv \
+    Results/11-fasda-kallisto/cond2-norm-$replicates.tsv

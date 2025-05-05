e552610147f87c4c0dcc79fdd5466842b23210b4
Jason Bacon <bacon@moray.acadix.biz>
2025-05-04 19:07:32 -0500

    11e-fasda-deseq2-overlap.sh: Report % overlap

--- Yeast/11e-fasda-deseq2-overlap.sh
+++ Yeast/11e-fasda-deseq2-overlap.sh
@@ -6,25 +6,29 @@ replicates=$(($samples / 2))
 output_dir=Results/11-fasda-kallisto
 fasda_de_file=$output_dir/fc-$replicates.txt
 
-
 printf "Total features:                   %s\n" \
 	$(wc -l $fasda_de_file | awk '{ print $1 }')
 
 awk '$1 != "Feature" && $8 < 0.05 { print $1 }' \
     $fasda_de_file | sort > fasda-sdegs.txt
-printf "SDE features reported by FASDA:   %s\n" \
-    $(wc -l fasda-sdegs.txt | awk '{ print $1 }')
+fasda_sde=$(wc -l fasda-sdegs.txt | awk '{ print $1 }')
+printf "SDE features reported by FASDA:   %s\n" $fasda_sde
 
 deseq_de_file=kallisto-deseq2-results.tsv
 awk '$1 != "baseMean" && $6 < 0.05 { print $1 }' \
     $deseq_de_file | sort > deseq-sdegs.txt
-printf "SDE features reported by DESeqs:  %s\n" \
-    $(wc -l deseq-sdegs.txt | awk '{ print $1 }')
+deseq2_sde=$(wc -l deseq-sdegs.txt | awk '{ print $1 }')
+printf "SDE features reported by DESeqs:  %s\n" $deseq2_sde
 
-printf "\nCommon to FASDA and DESeq2:       %s\n" \
-	$(comm -12 fasda-sdegs.txt deseq-sdegs.txt | wc -l)
-printf "Reported only by FASDA:           %s\n" \
-	$(comm -23 fasda-sdegs.txt deseq-sdegs.txt | wc -l)
-printf "Reported only by DESeq2:          %s\n" \
-	$(comm -13 fasda-sdegs.txt deseq-sdegs.txt | wc -l)
+common=$(comm -12 fasda-sdegs.txt deseq-sdegs.txt | wc -l)
+printf "\nCommon to FASDA and DESeq2:       %s\n" $common
+	
+fasda_only=$(comm -23 fasda-sdegs.txt deseq-sdegs.txt | wc -l)
+printf "Reported only by FASDA:           %s\n" $fasda_only
 
+deseq2_only=$(comm -13 fasda-sdegs.txt deseq-sdegs.txt | wc -l)
+printf "Reported only by DESeq2:          %s\n" $deseq2_only
+
+overlap=$((100 * $common / ($fasda_sde + $deseq2_sde) * 2))
+printf "\nOverlap = Common-SDE / ((FASDA-SDE + DESeq2-SDE)/2)\n"
+printf "Overlap:                          %s%%\n" $overlap

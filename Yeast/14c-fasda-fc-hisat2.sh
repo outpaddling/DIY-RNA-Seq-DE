f737b8c145004d023e2b4285035cb4357ea22671
Jason W. Bacon <bacon4000@gmail.com>
2026-06-08 08:47:43 -0500

    FASDA Hisat2: Generalize scripts for any number of replicates

--- Yeast/14c-fasda-fc-hisat2.sh
+++ Yeast/14c-fasda-fc-hisat2.sh
@@ -1,34 +1,45 @@
 #!/bin/sh -e
 
+##########################################################################
+#   Description:
+#       Run fasda normalize and fold-change on kallisto abundances
+##########################################################################
+
 input_dir=Results/13-hisat2-align
 samples=$(ls $input_dir/*.bam | wc -l)
 replicates=$(($samples / 2))
-
-output_dir=Results/14-fasda-hisat2
-mkdir -p $output_dir
-cd $output_dir
 
 uname -a
 fasda --version
 pwd
 
+output_dir=Results/14-fasda-hisat2
+mkdir -p $output_dir
+cd $output_dir
+
 # Separate counts by condition for fasda fold-change
 norm_all=all-norm-$replicates.tsv
+
+cols=$(awk 'NR == 1 { print NF }' $norm_all)
+last_c1_col=$(($cols / 2 + 1))
+first_c2_col=$(($last_c1_col + 1))
+echo $cols $last_c1_col $first_c2_col
+
 printf "\nCondition 1 normalized counts:\n"
-norm_file1=cond1-norm.tsv
-cut -f 1-4 $norm_all > $norm_file1
+norm_file1=cond1-norm-$replicates.tsv
+cut -f 1-$last_c1_col $norm_all > $norm_file1
 head -n 5 $norm_file1
 
 printf "\nCondition 2 normalized counts:\n"
-norm_file2=cond2-norm.tsv
-cut -f 1,5-7 $norm_all > $norm_file2
+norm_file2=cond2-norm-$replicates.tsv
+cut -f 1,$first_c2_col-$cols $norm_all > $norm_file2
 head -n 5 $norm_file2
 
-outfile=fc-3.txt
+outfile=fc-$replicates.txt
 # export PATH=~/Prog/Src/local/bin:$PATH
 printf "\nComputing fold-change for $replicates replicates...\n"
 echo "fasda fold-change --output $outfile cond1-norm-$replicates.tsv cond2-norm-$replicates.tsv"
-fasda fold-change --output $outfile cond1-norm.tsv cond2-norm.tsv
+fasda fold-change --output $outfile $norm_file1 $norm_file2
 
 more $outfile
 printf "\n%-25s %10s %10s\n" "File" "Features" "P < 0.05"
--- Yeast/14d-fasda-hisat2-heatmap.sh
+++ Yeast/14d-fasda-hisat2-heatmap.sh
@@ -9,13 +9,17 @@
 #   2025-03-31  Jason Bacon Begin
 ##########################################################################
 
+input_dir=Results/13-hisat2-align
+samples=$(ls $input_dir/*.bam | wc -l)
+replicates=$(($samples / 2))
+
 # Generate a list of features of interest by filtering the FASDA fold-changes
 features=filtered-features.txt
-fasda filter --max-p-val 0.05 Results/14-fasda-hisat2/fc-3.txt \
+fasda filter --max-p-val 0.05 Results/14-fasda-hisat2/fc-$replicates.txt \
     | awk '{ print $1 }' | head -n 30 > $features
 wc -l $features
 
 # FIXME: Update when ./heatmap is moved into PATH
 # Add --debug to see Python data structures
-fasda heatmap $features Results/14-fasda-hisat2/cond1-norm.tsv \
-    Results/14-fasda-hisat2/cond2-norm.tsv
+fasda heatmap $features Results/14-fasda-hisat2/cond1-norm-$replicates.tsv \
+    Results/14-fasda-hisat2/cond2-norm-$replicates.tsv
